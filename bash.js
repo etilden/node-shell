00f285d9a8ac0f442e3cd747c58ee99a491b6dbf
Emma Tilden <e.k.tilden@gmail.com>
2019-11-11 12:11:16 -0600

    last push before lunch

--- bash.js
+++ bash.js
@@ -1,10 +1,6 @@
+const pwdExport = require('./pwd.js')
+const ls = require('./ls.js'); 
 process.stdout.write('enter your first thing here > ');
-process.stdin.on('data', data => {
-  const cmd = data.toString().trim()
-  if(cmd === 'pwd') {
-    process.stdout.write(process.cwd())
-  }
 
-  process.stdout.write('\nprompt > ')
-})
-
+pwdExport()
+ls(); 
